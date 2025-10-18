b59dd7e13af8c3bc3b4d01876a6a61cbea8ab9ff
tejjm <uttej.madhusudhan@gmail.com>
2025-10-18 18:59:06 +0000

    Updated code to return character dictionary

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # python
 
-from stats import get_num_words
+from stats import get_num_words,count_char
 
 def main():
     path = "books/frankenstein.txt"
     num_words = get_num_words(path)
     print(f"Found {num_words} total words")
+    word_count = count_char(path)
+    print(word_count)
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,26 @@ def get_num_words(file_path):
     split_text = text.split()
     num_words = len(split_text)
     return num_words
+
+def count_char(file_path):
+    with open(file_path) as f:
+        text = f.read()
+    words = text.split()
+    lower_case = []
+    for word in words:
+        l_word = word.lower()
+        lower_case.append(l_word)
+    one_word = ""
+    for word in lower_case:
+        one_word+=word
+    char_count = {}
+    count = 0
+    for char in one_word:
+        if char not in char_count:
+            count = 1
+            char_count[char] = count
+            count = 0
+        elif char in char_count:
+            count = char_count[char]+1
+            char_count[char] = count
+    return char_count
