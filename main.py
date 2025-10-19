8af207f03a363ff5ec302e33fa24ac5b2af97461
tejjm <uttej.madhusudhan@gmail.com>
2025-10-19 08:36:25 +0000

    The code now accepts file paths as argument when running from terminal and can be used for different books

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 # python
-
+import sys
 from stats import get_num_words,count_char,sorted_list
 
 def main():
-    print("============ BOOKBOT ============")
-    path = "books/frankenstein.txt"
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    num_words = get_num_words(path)
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    word_count = count_char(path)
-    sorted_list(word_count)
-    print("============= END ===============")
-
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]
+        print("============ BOOKBOT ============")
+        # path = "books/frankenstein.txt"
+        print(f"Analyzing book found at {path}...")
+        print("----------- Word Count ----------")
+        num_words = get_num_words(path)
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        word_count = count_char(path)
+        sorted_list(word_count)
+        print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -6,30 +6,36 @@ def get_num_words(file_path):
     return num_words
 
 def count_char(file_path):
-    with open(file_path) as f:
-        text = f.read()
-    words = text.split()
-    lower_case = [word.lower() for word in words]
-    # one_word = ""
-    one_word = ''.join(word for word in lower_case)
     char_count = {}
-    count = 0
-    for char in one_word:
-        if char not in char_count:
-            count = 1
-            char_count[char] = count
-            count = 0
-        elif char in char_count:
-            count = char_count[char]+1
-            char_count[char] = count
+    with open(file_path) as f:
+        for ch in f.read().lower():
+            if "a" <= ch <= "z":
+                char_count[ch] = char_count.get(ch,0)+1
+
+
+    #     text = f.read()
+    # text = text.lower()
+    # new_text = ""
+    # for char in text:
+    #     if "a" <= char <= "z":
+    #         new_text+=char
+    # for char in new_text:
+    #     if char not in char_count:
+    #         count = 1
+    #         char_count[char] = count
+    #         count = 0
+    #     elif char in char_count:
+    #         count = char_count[char]+1
+    #         char_count[char] = count
     return char_count
+
 
 def sort_on(items):
     return items["num"]
 def sorted_list(dictionary):
     list = []
     list = [{"char":key,"num":value} for key,value in dictionary.items()]
-    list.sort(reverse=True,key=sort_on)
+    list.sort(key=lambda x: x["num"], reverse=True)
     for dict in list:
-        if dict["char"].isalpha():
-            print(f"{dict["char"]}: {dict["num"]}")
+        if "a" <= dict["char"] <= "z":
+            print(f"{dict['char']}: {dict['num']}")
